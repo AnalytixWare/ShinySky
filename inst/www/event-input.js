c1882ba1d460d4c656225b9064d5adcd3cea0f27
ZJ <zhuojia.dai@gmail.com>
2013-11-29 18:02:28 +0800

    Create event-input.js
    event input allows shiny server to reactive to anyevent.

---
+++ inst/www/event-input.js
@@ -0,0 +1,34 @@
+
+var eventInputBinding = new Shiny.InputBinding();
+$.extend(eventInputBinding, {
+  find: function(scope) {
+    return $(scope).find(".shinysky-eventinput");
+  },
+  getValue: function(el) {
+    var elem = $(el)
+    return {event : elem.data("shinysky-last-event"), event_count : elem.data("shinysky-event-count"), tot_event_count : elem.data("shinysky-tot-event-count")}
+  },
+  subscribe: function(el, callback) {
+
+  	//figure_out the events being monitored
+    var elem = $(el)
+     events = elem.data("shinysky-monitored-events").split(" ") //["click","dblclick", "hover"] 
+
+    events.map(function(x){
+      $(el).on(x + ".shinysky-eventinput", function(e) {
+        // some code here to change the tags
+        $(el).data("shinysky-last-event",x)
+        $(el).data("shinysky-event-count",parseInt($(el).data("shinysky-event-count"))+1)
+        $(el).data("shinysky-tot-event-count",parseInt($(el).data("shinysky-tot-event-count"))+1)
+        callback();
+      });    
+    })
+  },
+  unsubscribe: function(el) {
+    $(el).off(".shinysky-eventinput");
+  }
+});
+
+Shiny.inputBindings.register(eventInputBinding);
+
+154720
