58be8aef508681291765bc652cae910038e77ec8
ZJ <zhuojia.dai@gmail.com>
2014-01-01 19:38:06 +0800

    better ui layout

--- inst/examples/ui.R
+++ inst/examples/ui.R
@@ -4,75 +4,75 @@ library(shinysky)
 # Define UI for miles per gallon application
 shinyUI(basicPage(
   # Application title
-  headerPanel("ShinySky Examples"),  
+  headerPanel("ShinySky Examples"),  br(), br(),
   h4("ActionButtons"),
-  div(class="row-fluid well",
-  	div(class="container span3",
-  	  actionButton("id_blank","blank",size="large"),
-  	  actionButton("id_primary","primary",styleclass="primary",size="mini"),
-  	  actionButton("id_info","info",styleclass="info",size="small"),
-  	  actionButton("id_success","success",styleclass="success",icon = "ok"),
-  	  actionButton("id_warning","warning",styleclass="warning",icon="plus"),
-  	  actionButton("id_danger","danger",styleclass="danger"),
-  	  actionButton("id_inverse","inverse",styleclass="inverse"),
-  	  actionButton("id_link","link",styleclass="link")	  
-  		),
-   	div(class=" span3","Buttons that fill a block",
-   		actionButton("id_inverse2","inverse2",styleclass="inverse",block=T),
-   		actionButton("id_warning2","warning2",styleclass="warning",block=T)),
-  	div(class="container-fluid span6", 
-  		shiny::helpText("Click any button to show an alert"),
-  		shinyalert("shinyalert1")
-	)),
+  div(class="row-fluid",
+  div(class="well container-fluid" , div(class="container span3",
+        actionButton("id_blank","blank",size="large"),
+        actionButton("id_primary","primary",styleclass="primary",size="mini"),
+        actionButton("id_info","info",styleclass="info",size="small"),
+        actionButton("id_success","success",styleclass="success",icon = "ok"),
+        actionButton("id_warning","warning",styleclass="warning",icon="plus"),
+        actionButton("id_danger","danger",styleclass="danger"),
+        actionButton("id_inverse","inverse",styleclass="inverse"),
+        actionButton("id_link","link",styleclass="link")    
+        ),
+       div(class=" span3","Buttons that fill a block",
+         actionButton("id_inverse2","inverse2",styleclass="inverse",block=T),
+         actionButton("id_warning2","warning2",styleclass="warning",block=T)),
+      div(class="container-fluid span6", 
+        shiny::helpText("Click any button to show an alert"),
+        shinyalert("shinyalert1")
+    ))),
 
 
       h4("EventsButtons"),
 
-  	div(class="row-fluid well",
-	  	div(class="container span2",
-	  		eventsButton("id_double_click_event","Double click me!",styleclass="danger",events=c("dblclick","mouseenter"))
-	  	),
-  		div(class="container span10",
-  			shinyalert("shinyalert2")
-        )
+  	div(class="row-fluid",
+	  	div(class="container-fluid well",div(class="container span2",
+              eventsButton("id_double_click_event","Double click me!",styleclass="danger",events=c("dblclick","mouseenter"))
+            ),
+            div(class="container span10",
+              shinyalert("shinyalert2")
+              ))
   		)
     ,br()
   ,h4("Typeahead Text Input ")
-  ,div(class="row-fluid well",
-  	 div(class="container span3",
-	  	 textInput.typeahead(
-	    	id="thti"
-	    	,placeholder="type 'name' or '2'"
-	    	,local=data.frame(name=c("name1","name2"),info=c("info1","info2"))
-	    	,valueKey = "name"
-	    	,tokens=c(1,2)
-	    	,template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p> <p class='repo-description'>You need to learn more CSS to customize this further</p>")
-		  )
-  	 ),
-  	 div(class="container span9"
-  	 	,shiny::helpText("Type 'name' or '2' to see the features. ")
-  	 	,shinyalert("shinyalert3")
-  	 )
+  ,div(class="row-fluid ",
+div(class="well container-fluid",     div(class="container span3",
+      helpText("Type 'name' or '2' to see the features. "),
+       textInput.typeahead(
+        id="thti"
+        ,placeholder="type 'name' or '2'"
+        ,local=data.frame(name=c("name1","name2"),info=c("info1","info2"))
+        ,valueKey = "name"
+        ,tokens=c(1,2)
+        ,template = HTML("<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{name}}</p> <p class='repo-description'>You need to learn more CSS to customize this further</p>")
+      )
+     ),
+     div(class="container span9"
+       ,shinyalert("shinyalert3")
+     ))
   )
   ,h4("Select2")
-  ,div(class="row-fluid well",
-       div(class="container span3",
-         select2Input("select2Input1","This is a multiple select2Input",choices=c("a","b","c"),selected=c("b","a"))
-       ),
-       div(class="container span3"
-           ,shiny::helpText("Select2Input")
-           ,shinyalert("shinyalert4")
-       ),
-       div(class="container span3",
-         select2Input("select2Input2","This is a multiple select2Input type = select",choices=c("a","b","c"),selected=c("b","a"),type="select",multiple=TRUE)
-       ),
-       div(class="container span3"
-           ,shiny::helpText("Select2Input2")
-           ,shinyalert("shinyalert5")
-       )
+  ,div(class="row-fluid ",
+  div(class="well container-fluid"   ,  div(class="container span3",
+           select2Input("select2Input1","This is a multiple select2Input",choices=c("a","b","c"),selected=c("b","a"))
+         ),
+         div(class="container span3"
+             ,shiny::helpText("Select2Input")
+             ,shinyalert("shinyalert4")
+         ),
+         div(class="container span3",
+           select2Input("select2Input2","This is a multiple select2Input type = select",choices=c("a","b","c"),selected=c("b","a"),type="select",multiple=TRUE)
+         ),
+         div(class="container span3"
+             ,shiny::helpText("Select2Input2")
+             ,shinyalert("shinyalert5")
+         ))
 
   )
-  ,div(class="container span12"
+  ,div(class="container-fluid"
   	,h3("Source Code")
   	,tags$script(src="https://gist.github.com/xiaodaigh/7707701.js")
   	)
