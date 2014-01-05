dd3d1e35ee43443f9e29bf007d3464c0d7ac629f
Dai Zhuo Jia <zhuojia.dai@gmail.com>
2014-01-05 23:59:50 +0800

    gigthub pages create

--- R/pop-over.R
+++
@@ -1,8 +0,0 @@
-popover_instruction <- function(id, content) {
-    tagList(singleton(tags$head(tags$script(src = "bootstrap-tooltip.js"))), 
-        singleton(tags$head(tags$script(src = "bootstrap-popover.js"))), 
-        HTML(paste("\n    \n      <a id = \"", id, "\" href=\"#\" class=\"btn\" data-toggle=\"popover\" \n      title=\"Instructions\" \n      data-content=\"", 
-            content, "\" data-original-title=\"A Title\"><i class=\" icon-info-sign\"></i> Instructions</a>\n      <script> $(\"#", 
-            id, "\").popover({placement:\"bottom\"}) </script>\n        ", 
-            sep = "")))
-} 
--- R/run-shiny-sky-example.R
+++
@@ -1,6 +0,0 @@
-#' run.shinysky.example 
-#'
-#' @export
-run.shinysky.example <- function() {
-    shiny::runApp(system.file("examples", package = "shinysky"))
-} 
