b7390e5bd2ee74b900823df564b18cef1da4bf59
Dai Zhuo Jia <zhuojia.dai@gmail.com>
2014-04-12 11:51:59 +0800

    jstree begin

---
+++ R/jstree.R
@@ -0,0 +1,31 @@
+#' jstree.obj
+#' 
+#' Creates a jstree object to be passed to jstree for creating a tree view
+#' 
+#' @param id The id used to refer to the jstree
+#'   
+#' @export
+jstree.obj <- function(id, tree) {
+    tagList(        
+        singleton(tags$head(tags$link(href = "shinysky/jstree/dist/themes/default/style.min.css", rel = "stylesheet"))),
+        singleton(tags$head(tags$script(src = "shinysky/jstree/dist/jstree.min.js"))),
+        tags$script(sprintf("$(function() {$('#%s').jstree()",id)),
+        div(id = id, class = "ss-jstree")
+        )   
+}
+
+#' jstree
+#' 
+#' Creates a jstree
+#' 
+#' @param id The id used to refer to the jstree
+#'   
+#' @export
+jstree <- function(id, tree) {
+    tagList(        
+        singleton(tags$head(tags$link(href = "shinysky/jstree/dist/themes/default/style.min.css", rel = "stylesheet"))),
+        singleton(tags$head(tags$script(src = "shinysky/jstree/dist/jstree.min.js"))),
+        tags$script(sprintf("$(function() {$('#%s').jstree()",id)),
+        div(id = id, class = "ss-jstree")
+        )   
+}
