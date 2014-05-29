80fbf299230fdc2225ccdb148bb2ea8541cb536b
Roger Day <notsofeo@gmail.com>
2014-05-29 14:04:48 -0400

    Added parameter addLevelClass to jstree(); if true, add a class attribute "level_n" to each node, where n is the tree level (depth) of each node, starting with top level = 0.  Useful with conditionalPanel, css and jQuery.

--- R/jstree.R
+++ R/jstree.R
@@ -3,25 +3,36 @@
 #' Creates a jstree object to be passed to jstree for creating a tree view
 #' 
 #' @param id The id used to refer to the jstree
-#'   
+#' @param addLevelClass Add a class attribute "level_n" to each node, where n is the tree level (depth) of each node, starting with top level = 0.  Useful with conditionalPanel, css and jQuery.
+#' @param level The current tree level for this node, to be passed into the recursive calls. Default = 0 (top level). Not intended for the user; used only in the recursion.
 #' @export
-jstree.obj <- function(x) {  
-  handle <- function(ind, theList) { 
-    name<-names(theList)[[ind]]
-    if(!is.null(name)){
-      a<- tags$li(list(name, jstree.obj(theList[[ind]])) )
-    } else{
+jstree.obj = 
+function (x, addLevelClass=TRUE, level=0)
+{
+  handle <- function(ind, theList, level) {
+    name <- names(theList)[[ind]]
+    if (!is.null(name)) {
+      a <- tags$li(list(name, 
+		jstree.obj(theList[[ind]], 
+                        addLevelClass=addLevelClass,
+			level=level+1
+	)))
+    }
+    else {
       a <- tags$li(theList[[ind]])
     }
+    ### This line is added to shinysky:::jstree.obj
+    a <- tagAppendAttributes(a, class=paste0("level_", level))
     return(a)
   }
-  if(is.list(x)) {
+  if (is.list(x)) {
     ind <- seq(1:length(x))
-    res<- lapply(ind,handle,x)
+    res <- lapply(ind, handle, x, level=level)
     return(tags$ul(res))
-  } else {
+  }
+  else {
     x
-  }  
+  }
 }
 
 #' jstree
