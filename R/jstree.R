7ee8df54e9ca59197459da5378bd11c618856d98
Roger Day <notsofeo@gmail.com>
2014-05-31 14:12:54 -0400

    Changed class prefix to treedepth; removed (for now) addLevelType.

--- R/jstree.R
+++ R/jstree.R
@@ -3,11 +3,12 @@
 #' Creates a jstree object to be passed to jstree for creating a tree view
 #' 
 #' @param id The id used to refer to the jstree
-#' @param addLevelClass Add a class attribute "level_n" to each node, where n is the tree level (depth) of each node, starting with top level = 0.  Useful with conditionalPanel, css and jQuery.
+#' @param addLevelClass Add a class attribute "treedepth_n" to each node, where n is the tree level (depth) of each node, starting with top level = 1.  Useful with conditionalPanel, css and jQuery.
+#' @param addIndex Add an attrbute "index" to each node, a string containing the path from the top, in concatenated ind values. 
 #' @param level The current tree level for this node, to be passed into the recursive calls. Default = 0 (top level). Not intended for the user; used only in the recursion.
+#' @param index A string containing the path from the top, in concatenated ind values.  Not intended for the user; used only in the recursion.
 #' @export
-jstree.obj = 
-function (x, addLevelClass=TRUE, addLevelType=TRUE, addIndex=TRUE, level=0, index="0") 
+jstree.obj <- function (x, addLevelClass=TRUE, addIndex=TRUE, level=0, index="1") 
 {
   handle <- function(ind, theList, level) {
     name <- names(theList)[[ind]]
@@ -17,7 +18,6 @@ function (x, addLevelClass=TRUE, addLevelType=TRUE, addIndex=TRUE, level=0, inde
       a <- tags$li(list(name, 
                         myjstree.obj(theList[[ind]], 
                                      addLevelClass=addLevelClass,
-                                     addLevelType=addLevelType,
                                      addIndex=addIndex,
                                      level=level,
                                      index=index
@@ -26,14 +26,10 @@ function (x, addLevelClass=TRUE, addLevelType=TRUE, addIndex=TRUE, level=0, inde
     else {
       a <- tags$li(theList[[ind]])
     }
-    ### This line is added to shinysky:::jstree.obj
     if(addLevelClass)
-      a <- tagAppendAttributes(a, class=paste0("treeclass_", level))
-    if(addLevelType)
-      a <- tagAppendAttributes(a, type=paste0("level_", level))
+      a <- tagAppendAttributes(a, class=paste0("treedepth_", level))
     if(addIndex)
       a <- tagAppendAttributes(a, index=index)
-    # using the jstree plugin "types".
     return(a)
   }
   if (is.list(x)) {
