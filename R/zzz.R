d12dee45d331ec202001551cb70fa55d9f91e3cf
ZJ <zhuojia.dai@gmail.com>
2015-01-02 16:53:22 +1100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-#     require(shiny)
-#     require(RJSONIO)
     addResourcePath("shinysky", system.file("www", package = "shinysky"))
 }
-
-.onAttach <- function(libname, pkgname) {
-#     require(shiny)    
-} 
